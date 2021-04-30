fa468d24553a910e3b83aadfc4c4b0be21ec13e3
黄照 <1175019411@qq.com>
2021-04-30 19:03:54 +0800

    probject

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="hello">
-    11
     <div>{{ $store.state.name }}</div>
+    <div>{{ $store.state.Number }}</div>
+    <div>{{ vuexx }}</div>
+    <div>{{ name }}</div>
+    <div>{{ $store.getters.filterList }}</div>
+    <div>{{ filterList }}</div>
     <button @click="addState">修改state的值</button>
+    <button @click="addMapState(5)">修改Number的值</button>
+    <button @click="asyncClick()">async</button>
+    <button @click="addAsync(9)">用辅助函数</button>
   </div>
 </template>
 
 <script>
-
+import { mapMutations, mapState, mapActions, mapGetters } from 'vuex'
 export default {
   name: 'HelloWorld',
   props: {
@@ -15,7 +22,19 @@ export default {
   methods: {
     addState () {
       this.$store.commit('addState')
-    }
+    },
+    asyncClick () {
+      // 通过dispatch触发actions里面的方法
+      this.$store.dispatch('addAsync', 11)
+    },
+    ...mapMutations(['addMapState']),
+    // 通过辅助函数触发actions里面的方法
+    ...mapActions(['addAsync'])
+  },
+  computed: {
+    ...mapState(['vuexx', 'name']),
+    // 通过辅助函数写入getters里面的值
+    ...mapGetters(['filterList'])
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,16 +5,34 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    name: 'zs'
+    name: 'zs',
+    Number: 0,
+    vuexx: 'ls',
+    list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   },
   mutations: {
     // 修改state中的数据
     addState (state) {
       state.name += 1
+    },
+    addMapState (state, topLoad) {
+      state.Number += topLoad
     }
   },
   actions: {
+    addAsync (context, params) {
+      setTimeout(() => {
+        context.commit('addMapState', params)
+      }, 1000)
+    }
+  },
+  getters: {
+    filterList: state => state.list.filter(item => item > 5)
   },
   modules: {
+    // filter: function (state) {
+    //   return state.list.filter(item => item > 5)
+    // }
+    // filter: state => state.list.filter(item => item > 5)
   }
 })
